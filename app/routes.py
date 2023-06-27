52b81f1af08bb46aec1c9ee58890d2dfb51f28f3
Agnes Kim <agneskim4@gmail.com>
2023-06-27 16:17:08 -0400

    update units

--- app/routes.py
+++ app/routes.py
@@ -34,7 +34,7 @@ def get_weather():
 
     response = requests.get(
         "https://api.openweathermap.org/data/2.5/weather",
-        params={"lat": lat_query, "lon": lon_query, "appid": weather_key}
+        params={"lat": lat_query, "lon": lon_query, "appid": weather_key, "units": "imperial"}
     )
     return response.json()
 
